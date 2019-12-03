56c7cefa3b876f5d9f362d5e1d5b39dcce7f9b4b
Björn Biege <info@leanferret.com>
2019-12-03 09:25:04 +0100

    Day 3-1: draw vector

--- 3/index.js
+++ 3/index.js
@@ -4,9 +4,30 @@
 
 
 
+const drawVector = vector => {
+    vector.forEach(line => {
+        let row = '';
+        line.forEach(point => {
+            row += point + ' ';
+        });
+        console.log(row);
+    });
+    return '';
+}
+
 const getDistance = (p1, q1, p2, q2) => { return (p2 - p1) + (q2 - q1) }
 
 // # 2-1
+
+// draw vector
+console.log(drawVector([
+    ['.', '0', '-', '+', '.'],
+    ['.', '.', '.', '|', '.'],
+    ['.', '.', '.', '|', '.'],
+    ['.', '.', '.', '|', '.'],
+    ['.', '.', '-', '+', '.'],
+    ['.', '.', '.', '.', '.']
+]));
 
 // distance
 console.log('Distance is :', getDistance(0, 0, 3, 3));
